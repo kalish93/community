baf0d0cce8a67f16bf375edd45701207617accd1
kalish93 <kaleabtekalign@gmail.com>
2022-04-01 16:10:32 +0300

    initial

--- orginalarryfromdoubled.py
+++ orginalarryfromdoubled.py
@@ -1,26 +1,42 @@
-def find(num):
-    num.sort()
-    o = []
-    test = num[:]
-    while len(num) > 2:
-        temp = num.pop(0)
-        for i in range(len(num)-1):
-            if num[i] == 2 * temp:
-                num.pop(i)
-                o.append(temp)
-                break
-    if len(num) == 2:      
-        if num[0] * 2 == num[1]:
-            o.append(num[0])
-    if len(o) == len(test)/2:
-       return o
-    else:
-        return []
-    
-        
+# def find(num):
+#     num.sort()
+#     o = []
+#     test = num[:]
+#     while len(num) > 2:
+#         temp = num.pop(0)
+#         for i in range(len(num)-1):
+#             if num[i] == 2 * temp:
+#                 num.pop(i)
+#                 o.append(temp)
+#                 break
+#     if len(num) == 2:      
+#         if num[0] * 2 == num[1]:
+#             o.append(num[0])
+#     if len(o) == len(test)/2:
+#        return o
+#     else:
+#         return []
 
+from collections import Counter
+
+
+class Solution:
+    def findOriginalArray(self, nums):
+        nums.sort()
+        counts = Counter(nums)
+        temp = []
+        for num in nums:
+            if counts[num] > 0:
+                counts[num] -= 1
+            else:
+                continue
+                
+            double = num * 2
+            if double in counts and counts[double] > 0:
+                temp.append(num)
+                counts[double] -=1
             
-    
-changed = [1,2,2,2,4,4]
-print(find(changed))
-
+        if len(temp) == len(nums)/2:
+            return temp
+        return []
+        
