43a65a4ee1313c84e5471af7c610747bd4b278f5
kalish93 <kaleabtekalign@gmail.com>
2022-08-17 20:14:51 +0300

    final answer

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -1,32 +1,23 @@
-
-
-def isValid(s: str) -> bool:
-    l = []
-    for i in s:
-        if i == '(' or i == '[' or i == '{':
-            l.append(i)
-        elif i == ')' and len(l) != 0:
-            if l[-1] == '(':
-                l.pop()
-        elif i == ']' and len(l) != 0 :
-            if l[-1] == '[':
-                l.pop()
-        elif i == '}' and len(l) != 0:
-            if l[-1] == '{':
-                l.pop()
-        elif i == ')' and len(l) == 0:
-            
-            break
-        elif i == ']' and len(l) == 0 :
-            break
-        elif i == '}' and len(l) == 0:
-            break
-        return False
-            
-        
-    if len(l) == 0:
-        return True
-    # return False
-
-s = '()'
-print(isValid(s))
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        input = list(s)
+        for i in input:
+            if i == '{' or i == '[' or i == '(':
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                elif i == '}':
+                    if stack[-1] == '{':
+                        stack.pop()
+                    else: return False
+                elif i == ']':
+                    if stack[-1] == '[':
+                        stack.pop()
+                    else: return False
+                elif i == ')':
+                    if stack[-1] == '(':
+                        stack.pop()
+                    else: return False
+        return len(stack) == 0
