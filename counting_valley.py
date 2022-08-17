9bad480c171aabd854f6d8b6bf84a6e612607ffc
kalish93 <kaleabtekalign@gmail.com>
2022-08-17 19:49:39 +0300

    final answer

--- counting_valley.py
+++ counting_valley.py
@@ -1,23 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
 def countingValleys(steps, path):
-    # Write your code here
     count = 0
     valley = 0
     path = list(path)
     for i in path:
         if i == 'U':
             count+=1
-            if count == 0:
-                valley+=1
+        elif i == 'D' and count == 0:
+            count-=1
+            valley+=1
         elif i == 'D':
             count-=1
-            if count == 0:
-                valley+=1
-        # if count == 0:
-        #     valley+=1
     return valley
 
-path = 'UDDDUDUU'
-print(countingValleys(len(path),path))
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    steps = int(input().strip())
 
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
