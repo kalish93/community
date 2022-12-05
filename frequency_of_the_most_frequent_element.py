d3d042a9135ae2ec1b80beae18dbc5b46193f32e
kalish93 <91588962+kalish93@users.noreply.github.com>
2022-12-05 21:40:55 +0300

    Update frequency_of_the_most_frequent_element.py

--- frequency_of_the_most_frequent_element.py
+++ frequency_of_the_most_frequent_element.py
@@ -1,2 +1,14 @@
-def maxFreque(nums,k):
-    nums.sort()
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        maxfreq = 0
+        i,j = 0 , 0 
+        total = 0
+        nums.sort()
+        while j < len(nums):
+            total += nums[j]
+            while total + k < nums[j] * ( j - i + 1):
+                total -= nums[i]
+                i+=1
+            maxfreq = max(maxfreq, j - i + 1)
+            j+=1
+        return maxfreq
