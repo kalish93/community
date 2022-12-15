1cf452944f178a029a1f0ca7c6bc93486c930e8c
kalish93 <kaleabtekalign@gmail.com>
2022-12-15 21:56:19 +0300

    modified

--- middle-of-linked-list.py
+++ middle-of-linked-list.py
@@ -5,9 +5,23 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        j=head
-        i=head
-        while(j and j.next):
-            j = j.next.next
-            i = i.next
-        return i
+        # j=head
+        # i=head
+        # while(j and j.next):
+        #     j = j.next.next
+        #     i = i.next
+        # return i
+
+        temp=middlenode = head
+        count = 0
+        while(temp):
+            temp=temp.next
+            count +=1
+        middle = count//2
+        middlecount = 0
+        while(middlenode):
+            if middlecount == middle:
+                return middlenode
+            else:
+                middlecount+=1
+                middlenode = middlenode.next
