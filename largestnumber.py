a4c58ca73bff0fabf714c3b955fc92b60ee5d4dd
kalish93 <kaleabtekalign@gmail.com>
2022-12-30 11:47:07 +0300

    final commit

--- largestnumber.py
+++ largestnumber.py
@@ -1,52 +1,11 @@
-from tkinter import N
-
-
-def largestNumber(nums):
-    # for i in range(len(nums)):
-   
-#     print(nums)
-#     a = []
-#     b = []
-#     for i in range(len(nums)):
-#         temp = nums.pop()
-#         if temp > 10:
-#             a.append(temp)
-#         else:
-#             b.append(temp)
-#     output = ''
-#     for i in a:
-#         b.append(i)
-#     b.sort(reverse=True)
-#     for i in range(len(b)):
-#         output += str(b[i])
-#     return output
-    # for i in range(len(nums)):
-    #     nums[i] = str(nums[i])
-    # nums.sort(reverse=True)
-    # return nums
-    nums.sort()
-    a = []
-    for i in range(len(nums)-1):
-        if nums[i] == 10:
-            temp = nums.pop(i)
-            a.append(temp)
-    for i in range(len(nums)):
-        nums[i] = str(nums[i])
-    
-    nums.sort(reverse=True)
-    print(nums)
-    output = ''
-    for i in nums:
-        output += str(i)
-    for i in a:
-        output += str(i)
-    return output
-
-    
-nums = [33,30,34,5,9]
-nums.sort()
-print(nums)
-print(largestNumber(nums))
-# t = 78998
-# t = list(str(t))
-# print(t)
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        if sum(nums) == 0: return "0"
+        strnums = [str(num) for num in nums]
+        
+        for i in range(len(strnums)):
+            j = i+1
+            for j in range(len(strnums)-1):
+                if strnums[j] + strnums[i] < strnums[i] + strnums[j]:
+                    strnums[j] , strnums[i] = strnums[i] , strnums[j]
+        return "".join(map(str,strnums))
